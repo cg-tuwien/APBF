be0852368cf4a420c7839bf09d3ca510609e3aad
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2020-07-20 21:32:06 +0200

    Hello, World!

---
+++ source/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// stdafx.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.hpp"
+
+// TODO: reference any additional headers you need in stdafx.hpp
+// and not in this file
